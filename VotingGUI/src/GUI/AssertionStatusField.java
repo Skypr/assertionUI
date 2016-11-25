a09bd503861feb59e20a32c8d44229e4b86e6b3e
Lukas Stapelbroek <Lukas@Lukas-Laptop>
2016-11-25 17:05:22 +0100

    kleine Änderungen

--- VotingGUI/src/GUI/AssertionStatusField.java
+++ VotingGUI/src/GUI/AssertionStatusField.java
@@ -1,32 +1,50 @@
 package GUI;
 
+import java.awt.Checkbox;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;	
+import java.awt.event.WindowListener;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 
 public class AssertionStatusField extends JPanel implements WindowListener, ActionListener {
 
-	StartButton start = new StartButton();
+	JButton enlarge = new JButton();
+	
+	JButton checkBox = new JButton();
+	
+	JButton editButton = new JButton();
+	
 	private int id;
 	private AssertionContainer parent;
 	private boolean expanded = false;
 	private InfoTextField textField;
+	
+	
 
 	public AssertionStatusField(AssertionContainer parent, int id, int width, int height, int textHeight) {
+		this.setLayout(null);
 		this.parent = parent;
 		this.id = id;
-		this.setLayout(null);
-		this.setSize(width, height);
-		this.add(start);
 		this.setOpaque(true);
-		start.setLocation((getWidth() / 10) * 1, (getHeight() - start.getHeight()) / 2);
-
-		start.addActionListener(this);
+		this.setSize(width, height);
+		
+		enlarge.setSize(10, 10);
+		checkBox.setSize(10, 10);
+		editButton.setSize(50, 15);
+		
+		checkBox.setLocation((getWidth() / 10) * 8, (getHeight() - checkBox.getHeight()) / 2);
+		enlarge.setLocation((getWidth() / 10) * 1 , ((getHeight() - enlarge.getHeight()) / 10) * 9);
+		this.add(checkBox);
+		this.add(enlarge);
+		checkBox.setVisible(true);
+		enlarge.setVisible(true);
+		enlarge.addActionListener(this);
 		textField = new InfoTextField(width, textHeight);
 		textField.setSize(width, textHeight);
 		textField.setLocation(0, height);
